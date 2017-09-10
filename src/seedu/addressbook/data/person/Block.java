5323707945cc12240db10ad260b6a7755552d74e
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-09-10 21:32:55 +0800

    Split Address class into four sub-classes

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,4 +1,14 @@
 package seedu.addressbook.data.person;
 
 public class Block {
+
+    public final String value;
+
+    public Block(String block){
+        this.value = block;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
